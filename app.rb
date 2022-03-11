38f5655ce4d99e8f397105168386c1c9a98270bc
Edward Yara <83566118+oluyaratosin123@users.noreply.github.com>
2022-03-11 12:41:21 +0100

    Update app.rb

--- app.rb
+++ app.rb
@@ -121,18 +121,18 @@ class App
 
   def process_input(choice)
     case choice
-    when 1 
-      then list_all_books
-    when 2 
-      then list_all_people
-    when 3 
-      then create_a_person
-    when 4 
-      then create_a_book
-    when 5 
-      then create_a_rental
-    when 6 
-      then list_all_rentals
+    when 1 then list_all_books
+      
+    when 2 then list_all_people
+      
+    when 3 then create_a_person
+      
+    when 4 then create_a_book
+      
+    when 5 then create_a_rental
+    
+    when 6 then list_all_rentals
+      
     when 7 then
       puts "\nThank you for using School Library App. Goodbye!"
       exit
