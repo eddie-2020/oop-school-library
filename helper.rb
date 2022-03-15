94bd73fc39a0977d581acd07c706cf59d094115b
charlesgobina <charles.gobina@gmail.com>
2022-03-15 16:11:40 +0100

    Hosts all app methods

---
+++ helper.rb
@@ -0,0 +1,43 @@
+require_relative 'create'
+require_relative 'list'
+require 'pry'
+
+class Helper
+  include Create
+  include List
+
+  def initialize
+    @books = []
+    @person = []
+    @rentals = []
+
+  end
+
+  def leave
+    puts 'Thank you for using this app'
+    exit
+  end
+
+  # rubocop:disable Metrics/CyclomaticComplexity
+  def compute_choice(choice)
+    case choice
+    when 1
+      list_books
+    when 2
+      list_people
+    when 3
+      create_person
+    when 4
+      create_book
+    when 5
+      create_rental
+    when 6
+      list_rentals
+    when 7
+      @leave
+    else
+      puts 'Please enter the options listed in the menu'
+    end
+  end
+  # rubocop:enable Metrics/CyclomaticComplexity
+end
